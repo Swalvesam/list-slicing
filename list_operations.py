d5cb8bb0197ae20fb1d22b9cf5ef3376576f3639
Samantha Swalve <samantha.swalve@gmail.com>
2021-04-29 21:17:16 +0000

    enter operations to pass all tests

--- list_operations.py
+++ list_operations.py
@@ -36,7 +36,7 @@ def last(input_list):
 
     """
 
-    return input_list[-1:]
+    return input_list[-1]
 
 
 def top(input_list):
@@ -49,7 +49,7 @@ def top(input_list):
 
     """
 
-    return []
+    return input_list[0:-1]
 
 
 def first_three(input_list):
@@ -62,7 +62,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[0:3]
 
 
 def last_five(input_list):
@@ -75,7 +75,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -88,7 +88,9 @@ def middle(input_list):
 
     """
 
-    return []
+
+    return input_list[2:-2]
+
 
 
 def inner_four(input_list):
@@ -101,7 +103,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6:1]
 
 
 def inner_four_end(input_list):
@@ -117,7 +119,15 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
+    
+
+
+
+
+
+
+
 
 
 def replace_head(input_list):
@@ -131,7 +141,7 @@ def replace_head(input_list):
     True
 
     """
-
+    input_list[0] = 42
     pass
 
 
@@ -146,6 +156,8 @@ def replace_third_and_last(input_list):
     True
 
     """
+    input_list[2]= 37
+    input_list[-1]= 37
 
     pass
 
@@ -165,6 +177,7 @@ def replace_middle(input_list):
     True
 
     """
+    input_list[2:-2] = [42, 37]
 
     pass
 
@@ -180,6 +193,9 @@ def delete_third_and_seventh(input_list):
     True
 
     """
+    del input_list[2::4]
+    
+    
 
     pass
 
@@ -197,6 +213,7 @@ def delete_middle(input_list):
     True
 
     """
+    del input_list[2:-2]
 
     pass
 
